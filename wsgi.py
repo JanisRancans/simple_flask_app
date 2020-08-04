23df84135aefb78e241a8008592d773db942d3ee
JanisR <janis@JRs-MacBook-Pro.local>
2020-08-04 19:33:57 +0300

    First commit

---
+++ wsgi.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+
+application = Flask(__name__)
+
+@application.route("/")
+def hello_ip():
+    message = "Hello, {}".format(request.remote_addr)
+    return message
+
+
+if __name__ == "__main__":
+    application.run()
